7a5c41c591764a6a6e4edeb2a9b638f202d8813b
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2022-12-14 20:47:20 +0600

    error correction

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,8 @@
 const NotFound = ({ needFullHeight }) => {
   return (
-    <di className={`${needFullHeight && "min-h-scrren"} wrapper not-found`} >
+    <div className={`${needFullHeight && "min-h-scrren"} wrapper not-found`} >
       <h1>404<br />Not Found</h1>
-    </di>
+    </div>
   );
 };
 
